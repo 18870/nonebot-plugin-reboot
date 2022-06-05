4d07c4ff2d0289ee67466f47d1a15b62c2405003
18870 <a20110123@163.com>
2022-06-05 22:10:47 +0800

    Add: Plugin metadata

--- nonebot_plugin_reboot/__init__.py
+++ nonebot_plugin_reboot/__init__.py
@@ -1,3 +1,9 @@
+__plugin_adapters__ = []
+__plugin_name__ = "nonebot_plugin_reboot [重启 bot]"
+__plugin_des__ = "用命令重启机器人"
+__plugin_usage__ = ""
+__plugin_author__ = "18870 <a20110123@163.com>"
+
 from .reloader import Reloader
 from .config import plugin_config
 
