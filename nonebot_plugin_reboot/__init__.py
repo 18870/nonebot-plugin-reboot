343d9d960dd61fa3d8cf513e8b3c89c26f6b1387
18870 <a20110123@163.com>
2022-06-26 16:54:39 +0800

    Fix: Use wrong config in metadata

--- nonebot_plugin_reboot/__init__.py
+++ nonebot_plugin_reboot/__init__.py
@@ -1,20 +1,24 @@
-from nonebot.plugin import PluginMetadata
-from .config import plugin_config
+from .config import plugin_config, Config
 __plugin_adapters__ = []
 __plugin_name__ = "nonebot_plugin_reboot [重启 bot]"
 __plugin_des__ = "用命令重启机器人"
 __plugin_usage__ = ""
 __plugin_author__ = "18870 <a20110123@163.com>"
-__plugin_meta__ = PluginMetadata(
-    name=__plugin_name__,
-    description=__plugin_des__,
-    usage=__plugin_usage__,
-    config=plugin_config,
-    extra={
-        "author": __plugin_author__,
-        "adapters": __plugin_adapters__,
-    }
-)
+
+try:
+    from nonebot.plugin import PluginMetadata
+    __plugin_meta__ = PluginMetadata(
+        name=__plugin_name__,
+        description=__plugin_des__,
+        usage=__plugin_usage__,
+        config=Config,
+        extra={
+            "author": __plugin_author__,
+            "adapters": __plugin_adapters__,
+        }
+    )
+except ImportError:
+    pass
 
 from .reloader import Reloader
 
